4fe1321a7219dcf9fea7c8de532735e91d635348
timothee-oc <tmth.oc@gmail.com>
2025-01-31 10:14:06 +0100

    Catch IndexError in showSummary route if mail not found and send flash message in index.html template

--- server.py
+++ server.py
@@ -26,7 +26,11 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+    except IndexError:
+        flash("Sorry, that email wasn't found.")
+        return redirect(url_for("index"))
     return render_template('welcome.html',club=club,competitions=competitions)
 
 
