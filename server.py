f13e0bf6aa5f884b66774f47782c4e10f615b9a3
timothee-oc <tmth.oc@gmail.com>
2025-02-07 11:11:06 +0100

    remove a fix that I should have done in a separate branch

--- server.py
+++ server.py
@@ -65,7 +65,6 @@ def purchasePlaces():
         return render_template('booking.html',club=club,competition=competition)
     
     competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-    club['points'] = int(club['points']) - placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
